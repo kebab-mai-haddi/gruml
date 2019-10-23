39b927cd508469b84078d0fd9fa762f7d1b9d5eb
Aviral Srivastava <aviralsrivastava@dhcp-wifi-8021x-155-41-70-234.bu.edu>
2019-10-22 21:59:46 -0400

    plots inwards and outwards arrows across classes to indicate dependencies

--- inheritance_and_dependencies/plot_uml_in_excel.py
+++ inheritance_and_dependencies/plot_uml_in_excel.py
@@ -8,18 +8,6 @@ class WriteInExcel:
         self.file_name = file_name
         self.writer = pd.ExcelWriter(self.file_name, engine='xlsxwriter')
 
-    # def create_pandas_dataframe(self, base_class, children=[]):
-    #     number_of_rows = 1+len(children)
-    #     df = pd.DataFrame(index=np.arange(0, number_of_rows),
-    #                       columns=['Parent', 'Children'])
-    #     df.loc[0] = [base_class, '']
-    #     if number_of_rows == 1:
-    #         return df
-    #     for i in range(1, number_of_rows):
-    #         df.loc[i] = ['', children[-1]]
-    #         del children[-1]
-    #     return df
-
     def get_number_of_rows_in_df(self, agg_data):
         number_of_rows = 0
         for class_dict in agg_data:
@@ -27,49 +15,59 @@ class WriteInExcel:
                 len(class_dict['Methods']) + len(class_dict['Children'])
         return number_of_rows
 
-    def create_pandas_dataframe(self, agg_data):
+    def create_pandas_dataframe(self, agg_data, skip_cols):
         number_of_rows = self.get_number_of_rows_in_df(agg_data)
+        print('Number of rows in total: {}'.format(number_of_rows))
+        columns = ['' for _ in range(skip_cols)]
         df = pd.DataFrame(
             index=np.arange(0, number_of_rows),
-            columns=['Dependents', 'Parent', 'Methods/Children']
+            columns=columns+['Parent', 'Methods/Children']
         )
         row_counter = 0
+        column_counter = skip_cols-1
+        # mapping to store row, col for dependents.
+        dependents_col_counter = {}
+        class_row_mapping = {}  # class: row number mapping
         prev_class_row_counter = 0
         for class_data in agg_data:
             base_class = class_data['Class']
+            class_row_mapping[base_class] = row_counter
             methods = class_data['Methods']
             children = class_data['Children']
             dependents = class_data['Dependents']
-            df.loc[row_counter] = ['', base_class, '']
+            columns = ['' for _ in range(skip_cols)]
+            df.loc[row_counter] = columns + [base_class, '']
             row_counter += 1
             for method in methods:
-                df.at[row_counter, 'Methods/Children'] = method
+                print('Inserting method: {} of class: {} at row: {} and column: {}'.format(
+                    method, base_class, row_counter, skip_cols+1))
+                df.iloc[row_counter, skip_cols+1] = method
                 row_counter += 1
             for child in children:
-                df.at[row_counter, 'Methods/Children'] = child
+                df.iloc[row_counter, skip_cols+1] = child
                 row_counter += 1
             for dependent in dependents:
-                df.at[prev_class_row_counter+1, 'Dependents'] = dependent
-                row_counter += 1
+                df.iloc[prev_class_row_counter, column_counter] = "➜"
+                if dependents_col_counter.get(dependent, None):
+                    dependents_col_counter[dependent].append(column_counter)
+                else:
+                    dependents_col_counter[dependent] = [column_counter]
+                column_counter -= 1
             prev_class_row_counter = row_counter
+
+        for dependent in dependents_col_counter.keys():
+            for column in dependents_col_counter[dependent]:
+                df.iloc[class_row_mapping[dependent], column] = "←"
+
         # convert all NaN to None.
         df = df.replace(np.nan, '', regex=True)
         print("Create DF was called.")
         print(df)
         return df
 
-    # def form_uml_sheet_for_classes(self):
-    #     for key, value in self.classes.items():
-    #         base_class = key
-    #         children = value
-    #         class_df = self.create_pandas_dataframe(base_class, children)
-    #         self.write_df_to_excel(class_df, base_class)
-    #     self.writer.save()
-    #     self.writer.close()
-
-    def write_df_to_excel(self, df, sheet_name):
+    def write_df_to_excel(self, df, sheet_name, skip_cols=0):
         df.to_excel(self.writer, sheet_name=sheet_name,
-                    header=True, index=False, )
+                    header=True, index=False, startcol=skip_cols)
         self.writer.save()
         self.writer.close()
         print("{} done!".format(sheet_name))
