cfb770ec6bd7f936219c48a50ebffc18fb1d2339
Aviral Srivastava <avi.srivastava254084@gmail.com>
2020-04-13 10:44:19 -0400

    adds handling of str instance type of super classes: converts them to Class object in order to determine their module

--- generate_hierarchy.py
+++ generate_hierarchy.py
@@ -3,6 +3,7 @@ import logging
 import os
 import subprocess
 import sys
+from collections import defaultdict
 from operator import itemgetter
 
 logging.basicConfig(level=logging.DEBUG)
@@ -10,7 +11,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 class GenerateHierarchy:
     def __init__(self):
-        self.class_dict = {}  # it will have a class:children mapping.
+        # it will have a module:class:children mapping.
+        self.class_dict = defaultdict(lambda: defaultdict(list))
 
     def show_class(self, name, class_data):
         """show the class and its contents.
@@ -34,13 +36,12 @@ class GenerateHierarchy:
             list -- all the methods that are declared in the given class(name param)
         """
         methods = []
-        for name, _ in sorted(class_data.methods.items(),
-                              key=itemgetter(1)):
+        for name, _ in sorted(class_data.methods.items(), key=itemgetter(1)):
             # logging.debug('  Method: {0} [{1}]'.format(name, lineno))
             methods.append(name)
         return methods
 
-    def show_super_classes(self, name, class_data):
+    def show_super_classes(self, name, class_data, object):
         """Shows super classes for a given class
 
         Arguments:
@@ -55,18 +56,42 @@ class GenerateHierarchy:
             if super_class == 'object':
                 continue
             if isinstance(super_class, str):
-                super_class_names.append(super_class)
-            else:
-                super_class_names.append(super_class.name)
+                # find the class in covered modules
+                found = False
+                for module in object.class_object_mapping.keys():
+                    for class_ in object.class_object_mapping[module].keys():
+                        if object.class_object_mapping[module][class_].name == super_class:
+                            found = True
+                            super_class = object.class_object_mapping[module][class_]
+                            break
+                    if found:
+                        break
+            super_class_names.append(
+                {
+                    'parent_module': super_class.module,
+                    'parent_class': super_class.name
+                }
+            )
         for super_class_name in super_class_names:
-            if self.class_dict.get(super_class_name, None):
-                self.class_dict[super_class_name].append(name)
+            if self.class_dict.get(super_class_name['parent_module']):
+                if self.class_dict.get(super_class_name['parent_class']):
+                    self.class_dict[super_class_name['parent_module']][super_class_name['parent_class']].append(
+                        {
+                            'child_module': class_data.module,
+                            'child_class': name
+                        }
+                    )
             else:
-                self.class_dict[super_class_name] = [name]
+                self.class_dict[super_class_name['parent_module']][super_class_name['parent_class']] = [
+                    {
+                        'child_module': class_data.module,
+                        'child_class': name
+                    }
+                ]
         # adding all parents for a class in one place for later usage: children
         return super_class_names
 
-    def get_children(self, name):
+    def get_children(self, name, class_data):
         """returns all the computed children for a given class if any
         otherwise returns an empty list.
 
@@ -76,6 +101,8 @@ class GenerateHierarchy:
         Returns:
             list -- list of classes that are children for a given class.
         """
-        if self.class_dict.get(name, None):
-            return self.class_dict[name]
+        module = class_data.module
+        if self.class_dict.get(module, None):
+            if self.class_dict[module].get(name):
+                return self.class_dict[class_data.module][name]
         return []
