f2528d3c1329d76d1584f3c529ae9f9d6515c5c7
Aviral Srivastava <avi.srivastava254084@gmail.com>
2020-06-08 10:05:36 -0400

    inserting source code base in the sys path

--- generate_sequence_diagram.py
+++ generate_sequence_diagram.py
@@ -8,7 +8,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class GenerateSequenceDiagram:
-    def __init__(self, driver_path, driver_module, source_code_to_study_dir):
+    def __init__(self, source_code_to_study_dir, driver_path=None, driver_module=None):
+        '''
+        source_code_to_study_dir
+        '''
         # self.driver_module = __import__(driver_module)
         self.driver_path = driver_path
         self.driver_module = driver_module
